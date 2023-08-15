82e3e4c388957bc1587946407115b7fb98d81bef
Ling Yang <yanglingit@outlook.com>
2023-08-15 19:07:42 +0800

    Initial commit.

---
+++ src/main/java/ling/yang/bugreportknife4j/BugreportKnife4jApplication.java
@@ -0,0 +1,13 @@
+package ling.yang.bugreportknife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugreportKnife4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugreportKnife4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ling/yang/bugreportknife4j/BugreportKnife4jApplicationTests.java
@@ -0,0 +1,13 @@
+package ling.yang.bugreportknife4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugreportKnife4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
